93e52201ec2d25ced47d538aacd287513672b7f1
Huikie <z4everaz@hotmail.com>
2019-06-04 17:50:41 +0200

    update

--- app/src/main/java/com/example/daan/bestpracticeecologicalfootprintforuniversities/CalculateEfActivity.java
+++ app/src/main/java/com/example/daan/bestpracticeecologicalfootprintforuniversities/CalculateEfActivity.java
@@ -1,287 +1,99 @@
 package com.example.daan.bestpracticeecologicalfootprintforuniversities;
 
-import android.content.Intent;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.text.DecimalFormat;
-
-import static android.view.View.GONE;
-import static android.view.View.VISIBLE;
+import android.widget.Spinner;
 
 public class CalculateEfActivity extends AppCompatActivity {
-
-    Float efpc_calc, energy_calc, mobility_calc, food_calc, waste_calc, water_calc, paper_calc, bl_calc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate_ef);
 
-//        if(savedInstanceState != null){
-//            Log.d("hier", "goed");
-//            TextView efpc = findViewById(R.id.efpc);
-//            efpc.setText(Html.fromHtml(savedInstanceState.getString("efpc",null)));
-//            EditText input_efpc = findViewById(R.id.input_efpc);
-//            String saved_efpc = savedInstanceState.getString("i_efpc", "");
-//            input_efpc.setText(saved_efpc);
-//        }
 
-        Intent intent = getIntent();
-        Float efpc = intent.getFloatExtra("efpc", 0);
-        Float energy = intent.getFloatExtra("energy", 0);
-        Float mobility = intent.getFloatExtra("mobility", 0);
-        Float waste = intent.getFloatExtra("waste", 0);
-        Float water = intent.getFloatExtra("water", 0);
-        Float food = intent.getFloatExtra("food", 0);
-        Float built_land = intent.getFloatExtra("built_land", 0);
-        Float paper = intent.getFloatExtra("paper", 0);
+//        Spinner BL_info = findViewById(R.id.BL_dd);
+//        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this,R.layout.spinner_textview,getResources().getStringArray(R.array.BL_info));
+//        myAdapter.setDropDownViewResource(R.layout.dropdown_item_textview);
+//        BL_info.setAdapter(myAdapter);
 
-        efpc_calc = efpc;
-        energy_calc = energy;
-        mobility_calc = mobility;
-        food_calc = food;
-        waste_calc = waste;
-        water_calc = water;
-        paper_calc = paper;
-        bl_calc = built_land;
     }
 
-//    @Override
-//    public void onSaveInstanceState(Bundle outState) {
-//        super.onSaveInstanceState(outState);
-//        Log.d("saved", "ja");
-//        TextView efpc = findViewById(R.id.efpc);
-//        TextView energy = findViewById(R.id.energy);
-//        TextView built_land = findViewById(R.id.built_land);
-//        TextView mobility = findViewById(R.id.mobility);
-//        TextView food = findViewById(R.id.food);
-//        TextView waste = findViewById(R.id.waste);
-//        TextView water = findViewById(R.id.water);
-//        TextView paper = findViewById(R.id.paper);
-//
-//        outState.putString("efpc", efpc.getText().toString());
-//        outState.putInt("arms", arms.getVisibility());
-//        outState.putInt("eyes", eyes.getVisibility());
-//        outState.putInt("eyebrows", eyebrows.getVisibility());
-//        outState.putInt("ears", ears.getVisibility());
-//        outState.putInt("mustache", mustache.getVisibility());
-//        outState.putInt("nose", nose.getVisibility());
-//        outState.putInt("shoes", shoes.getVisibility());
-//        outState.putInt("mouth", mouth.getVisibility());
-//        outState.putInt("glasses", glasses.getVisibility());
-//
-//        EditText input_energy = findViewById(R.id.input_energy);
-//        EditText input_efpc = findViewById(R.id.input_efpc);
-//        EditText input_built_land = findViewById(R.id.input_bl);
-//        EditText input_mobility = findViewById(R.id.input_mobility);
-//        EditText input_food = findViewById(R.id.input_food);
-//        EditText input_waste = findViewById(R.id.input_waste);
-//        EditText input_water = findViewById(R.id.input_water);
-//        EditText input_paper = findViewById(R.id.input_paper);
-//
-//        outState.putString("i_efpc", input_efpc.getText().toString());
-//    }
+    public void blCalc(View w) {
 
+        String[] items = new String[] {"Built land (m2) * 0,000283", "--------------------------------------------- +", "Total hectares / university's population"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, items);
 
-    public void CompareEf(View v){
-        try{
-            TextView efpc = findViewById(R.id.efpc);
-            TextView energy = findViewById(R.id.energy);
-            TextView built_land = findViewById(R.id.built_land);
-            TextView mobility = findViewById(R.id.mobility);
-            TextView food = findViewById(R.id.food);
-            TextView waste = findViewById(R.id.waste);
-            TextView water = findViewById(R.id.water);
-            TextView paper = findViewById(R.id.paper);
+        new AlertDialog.Builder(this)
+                .setTitle("Built land calculation")
+                .setAdapter(adapter, new DialogInterface.OnClickListener() {
 
-            efpc.setText("");
-            energy.setText("");
-            built_land.setText("");
-            mobility.setText("");
-            food.setText("");
-            waste.setText("");
-            water.setText("");
-            paper.setText("");
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-            EditText input_energy = findViewById(R.id.input_energy);
-            EditText input_efpc = findViewById(R.id.input_efpc);
-            EditText input_built_land = findViewById(R.id.input_bl);
-            EditText input_mobility = findViewById(R.id.input_mobility);
-            EditText input_food = findViewById(R.id.input_food);
-            EditText input_waste = findViewById(R.id.input_waste);
-            EditText input_water = findViewById(R.id.input_water);
-            EditText input_paper = findViewById(R.id.input_paper);
-
-            try{
-                if (!input_efpc.getText().toString().isEmpty()) {
-                    efpc.setVisibility(VISIBLE);
-                    Float efpc_f = Float.parseFloat(input_efpc.getText().toString());
-                    Log.d("input", input_efpc.getText().getClass().getName());
-                    Float end_efpc = Float.valueOf(new DecimalFormat("#.#").format((((efpc_calc - efpc_f) / efpc_calc) * 100)));
-                    if (end_efpc < 0) {
-                        efpc.setText(Html.fromHtml("Per capita % difference with BP <font color='red'>" + end_efpc.toString().substring(1) + "%</font>"));
-                    } else {
-                        efpc.setText(Html.fromHtml("Per capita % difference with BP <font color='green'>" + end_efpc + "%</font>"));
+                        dialog.dismiss();
                     }
-                }
-                if (input_efpc.getText().toString().isEmpty()) {
-                    efpc.setVisibility(GONE);
-                }
+                }).create().show();
+    }
 
-            }catch(NumberFormatException e){
-                input_efpc.setVisibility(GONE);
-                input_efpc.setError("Make sure floating input is written with a dot (.)!");
-                }
-                try{
-                    if (!input_energy.getText().toString().isEmpty()) {
-                        energy.setVisibility(VISIBLE);
-                        Float energy_f = Float.parseFloat(input_energy.getText().toString());
-                        Float end_energy = Float.valueOf(new DecimalFormat("#.#").format((((energy_calc - energy_f) / energy_calc) * 100)));
-                        if (end_energy < 0) {
-                            energy.setText(Html.fromHtml("Energy % difference with BP <font color='red'>" + end_energy.toString().substring(1) + "%</font>"));
-                        } else {
-                            energy.setText(Html.fromHtml("Energy % difference with BP <font color='green'>" + end_energy + "%</font>"));
-                        }
+    public void energyCalc(View w) {
+
+        String[] items = new String[] {"Natural gas (m3) * 0,00048885", "Wind (kWh) * 0,000024", "Sun (kWh) * 0,000024", "--------------------------------------------- +", "Total hectares / university's population"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, items);
+
+        new AlertDialog.Builder(this)
+                .setTitle("Energy calculation")
+                .setAdapter(adapter, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.dismiss();
                     }
+                }).create().show();
+    }
 
-                    if (input_energy.getText().toString().isEmpty()) {
-                        energy.setVisibility(GONE);
+    public void foodCalc(View w) {
+
+        String[] items = new String[] {"Fish & other seafood (kg) * 0,0227", "Grain (kg) * 0,0017","Vegetables (kg) * 0,0004","Diary (kg) * 0,00199","Fruit (kg) * 0,0005", "Meat (kg) * 0.0069", "-------------------------------------------- +", "Total hectares / university's population"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, items);
+
+        new AlertDialog.Builder(this)
+                .setTitle("Food calculation")
+                .setAdapter(adapter, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.dismiss();
                     }
-                }
-                catch(NumberFormatException e){
-                    energy.setVisibility(GONE);
-                    input_energy.setError("Make sure floating input is written with a dot (.)!");
-                }
+                }).create().show();
+    }
 
-                try {
-                    if (!input_mobility.getText().toString().isEmpty()) {
-                        mobility.setVisibility(VISIBLE);
-                        Float mobility_f = Float.parseFloat(input_mobility.getText().toString());
-                        Float end_mobility = Float.valueOf(new DecimalFormat("#.#").format((((mobility_calc - mobility_f) / mobility_calc) * 100)));
-                        if (end_mobility < 0) {
-                            mobility.setText(Html.fromHtml("Mobility % difference with BP <font color='red'>" + end_mobility.toString().substring(1) + "%</font>"));
-                        } else {
-                            mobility.setText(Html.fromHtml("Mobility % difference with BP <font color='green'>" + end_mobility + "%</font>"));
-                        }
+    public void mobilityCalc(View w) {
+
+        String[] items = new String[] {"Public transport (km) * 0,000011", "Car (km) * 0,0000395","Motorbike (km) * 0,0000129","Bicycle (km) * 0,0000026", "-------------------------------------------- +", "Total hectares / university's population"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, items);
+
+        new AlertDialog.Builder(this)
+                .setTitle("Mobility calculation")
+                .setAdapter(adapter, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.dismiss();
                     }
-
-                    if (input_mobility.getText().toString().isEmpty()) {
-                        mobility.setVisibility(GONE);
-                    }
-                }catch(NumberFormatException e){
-                    mobility.setVisibility(GONE);
-                    input_mobility.setError("Make sure floating input is written with a dot (.)!");
-                }
-
-                try {
-                    if (!input_food.getText().toString().isEmpty()) {
-                        food.setVisibility(VISIBLE);
-                        Float food_f = Float.parseFloat(input_food.getText().toString());
-                        Float end_food = Float.valueOf(new DecimalFormat("#.#").format((((food_calc - food_f) / food_calc) * 100)));
-                        if (end_food < 0) {
-                            food.setText(Html.fromHtml("Food % difference with BP <font color='red'>" + end_food.toString().substring(1) + "%</font>"));
-                        } else {
-                            food.setText(Html.fromHtml("Food % difference with BP <font color='green'>" + end_food + "%</font>"));
-                        }
-                    }
-
-                    if (input_food.getText().toString().isEmpty()) {
-                        food.setVisibility(GONE);
-                    }
-                }catch(NumberFormatException e){
-                    food.setVisibility(GONE);
-                    input_food.setError("Make sure floating input is written with a dot (.)!");
-                }
-
-                try{
-                if (!input_built_land.getText().toString().isEmpty()) {
-                    built_land.setVisibility(VISIBLE);
-                    Float built_land_f = Float.parseFloat(input_built_land.getText().toString());
-                    Float end_built_land = Float.valueOf(new DecimalFormat("#.#").format((((bl_calc - built_land_f) / bl_calc) * 100)));
-                    if (end_built_land < 0) {
-                        built_land.setText(Html.fromHtml("Built land % difference with BP <font color='red'>" + end_built_land.toString().substring(1) + "%</font>"));
-                    } else {
-                        built_land.setText(Html.fromHtml("Built land % difference with BP <font color='green'>" + end_built_land + "%</font>"));
-                    }
-                }
-
-                if (input_built_land.getText().toString().isEmpty()) {
-                built_land.setVisibility(GONE);
-                }
-
-                }catch(NumberFormatException e){
-                    built_land.setVisibility(GONE);
-                    input_built_land.setError("Make sure floating input is written with a dot (.)!");
-                }
-                try {
-                    if (!input_paper.getText().toString().isEmpty()) {
-                        paper.setVisibility(VISIBLE);
-                        Float paper_f = Float.parseFloat(input_paper.getText().toString());
-                        Float end_paper = Float.valueOf(new DecimalFormat("#.#").format((((paper_calc - paper_f) / paper_calc) * 100)));
-                        if (end_paper < 0) {
-                            paper.setText(Html.fromHtml("Paper % difference with BP <font color='red'>" + end_paper.toString().substring(1) + "%</font>"));
-                        } else {
-                            paper.setText(Html.fromHtml("Paper % difference with BP <font color='green'>" + end_paper + "%</font>"));
-                        }
-                    }
-
-                    if (input_paper.getText().toString().isEmpty()) {
-                        paper.setVisibility(GONE);
-                    }
-                }catch(NumberFormatException e){
-                    paper.setVisibility(GONE);
-                    input_paper.setError("Make sure floating input is written with a dot (.)!");
-                }
-
-                try {
-                    if (!input_waste.getText().toString().isEmpty()) {
-                        waste.setVisibility(VISIBLE);
-                        Float waste_f = Float.parseFloat(input_waste.getText().toString());
-                        Float end_waste = Float.valueOf(new DecimalFormat("#.#").format((((waste_calc - waste_f) / waste_calc) * 100)));
-                        if (end_waste < 0) {
-                            waste.setText(Html.fromHtml("Waste % difference with BP <font color='red'>" + end_waste.toString().substring(1) + "%</font>"));
-                        } else {
-                            waste.setText(Html.fromHtml("Waste % difference with BP <font color='green'>" + end_waste + "%</font>"));
-                        }
-                    }
-
-                    if (input_waste.getText().toString().isEmpty()) {
-                        waste.setVisibility(GONE);
-                    }
-                }catch(NumberFormatException e){
-                    waste.setVisibility(GONE);
-                    input_waste.setError("Make sure floating input is written with a dot (.)!");
-                }
-
-                try {
-                    if (!input_water.getText().toString().isEmpty()) {
-                        water.setVisibility(VISIBLE);
-                        Float water_f = Float.parseFloat(input_water.getText().toString());
-                        Float end_water = Float.valueOf(new DecimalFormat("#.#").format(((water_calc - water_f) / water_calc) * 100));
-                        if (end_water < 0) {
-                            water.setText(Html.fromHtml("Water % difference with BP <font color='red'>" + end_water.toString().substring(1) + "%</font>"));
-                        } else {
-                            water.setText(Html.fromHtml("Water % difference with BP <font color='green'>" + end_water + "%</font>"));
-                        }
-                    }
-
-                    if (input_water.getText().toString().isEmpty()) {
-                        water.setVisibility(GONE);
-                    }
-                }catch(NumberFormatException e){
-                    water.setVisibility(GONE);
-                    input_water.setError("Make sure floating input is written with a dot (.)!");
-                }
-        }
-        catch (Exception e){
-            Toast.makeText(this, "Something went wrong!",Toast.LENGTH_LONG).show();
-        }
+                }).create().show();
     }
 }
---
+++ app/src/main/java/com/example/daan/bestpracticeecologicalfootprintforuniversities/OverviewActivity.java
@@ -0,0 +1,153 @@
+package com.example.daan.bestpracticeecologicalfootprintforuniversities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class OverviewActivity extends AppCompatActivity implements UniversityInfoRequest.Callback {
+
+    public Float min_efpc = Float.parseFloat("1");
+    public Float min_energy = Float.parseFloat("1");
+    public Float min_paper = Float.parseFloat("1");
+    public Float min_food = Float.parseFloat("1");
+    public Float min_built_land = Float.parseFloat("1");
+    public Float min_water = Float.parseFloat("1");
+    public Float min_mobility = Float.parseFloat("1");
+    public Float min_waste = Float.parseFloat("1");
+    public String university_efpc, university_energy, university_paper, university_food, university_built_land, university_water, university_mobility, university_waste;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+
+        UniversityInfoRequest x = new UniversityInfoRequest(this);
+        x.getUniInfo(this);
+    }
+
+    @Override
+    public void gotUniInfo(ArrayList<University> universities) {
+        Toast.makeText(this, "succes", Toast.LENGTH_LONG).show();
+        try{
+            for(int i=0; i<universities.size();i++) {
+
+                String Efpc = universities.get(i).getEfpc();
+                if (!Efpc.isEmpty()) {
+                    Float Efpc_i = Float.parseFloat(Efpc);
+                    if (Efpc_i < min_efpc) {
+                        min_efpc = Efpc_i;
+                        university_efpc = universities.get(i).getName();
+                    }
+                }
+
+                String energy = universities.get(i).getEnergy();
+                if (!energy.isEmpty()) {
+                    Float energy_i = Float.parseFloat(energy);
+                    if (energy_i < min_energy) {
+                        min_energy = energy_i;
+                        university_energy = universities.get(i).getName();
+                    }
+                }
+
+                String paper = universities.get(i).getPaper();
+                if (!paper.isEmpty()) {
+                    Float paper_i = Float.parseFloat(paper);
+                    if (paper_i < min_paper) {
+                        min_paper = paper_i;
+                        university_paper = universities.get(i).getName();
+                    }
+                }
+
+                String food = universities.get(i).getFood();
+                if (!food.isEmpty()) {
+                    Float food_i = Float.parseFloat(food);
+                    if (food_i < min_food) {
+                        min_food = food_i;
+                        university_food = universities.get(i).getName();
+                    }
+                }
+
+                String built_land = universities.get(i).getBuilt_land();
+                if (!built_land.isEmpty()) {
+                    Float built_land_i = Float.parseFloat(built_land);
+                    if (built_land_i < min_built_land) {
+                        min_built_land = built_land_i;
+                        university_built_land = universities.get(i).getName();
+                    }
+                }
+
+                String water = universities.get(i).getWater();
+                if (!water.isEmpty()) {
+                    Float water_i = Float.parseFloat(water);
+                    if (water_i < min_water) {
+                        min_water = water_i;
+                        university_water = universities.get(i).getName();
+                    }
+                }
+
+                String mobility = universities.get(i).getMobility();
+                if (!mobility.isEmpty()) {
+                    Float mobility_i = Float.parseFloat(mobility);
+                    if (mobility_i < min_mobility) {
+                        min_mobility = mobility_i;
+                        university_mobility = universities.get(i).getName();
+                    }
+                }
+
+                String waste = universities.get(i).getWaste();
+                if (!waste.isEmpty()) {
+                    Float waste_i = Float.parseFloat(waste);
+                    if (waste_i < min_waste) {
+                        min_waste = waste_i;
+                        university_waste = universities.get(i).getName();
+                    }
+                }
+
+            }
+            ArrayList<University_bp> bp_unis = new ArrayList<>();
+            bp_unis.add(new University_bp(university_efpc, "<b>EF:</b> "+min_efpc));
+            bp_unis.add(new University_bp(university_energy, "<b>Energy EF:</b> "+min_energy));
+            bp_unis.add(new University_bp(university_paper, "<b>Paper EF:</b> "+min_paper));
+            bp_unis.add(new University_bp(university_food, "<b>Food EF:</b> "+min_food));
+            bp_unis.add(new University_bp(university_built_land,"<b>Built land EF:</b> "+min_built_land));
+            bp_unis.add(new University_bp(university_water, "<b>Water EF:</b> "+min_water));
+            bp_unis.add(new University_bp(university_mobility, "<b>Mobility EF:</b> "+min_mobility));
+            bp_unis.add(new University_bp(university_waste, "<b>Waste EF:</b> "+min_waste));
+
+            // Sort the spots in alphabetic order based on the spot name.
+            Collections.sort(bp_unis);
+
+            BpUniAdapter adapter = new BpUniAdapter(this, R.layout.bp_item, bp_unis);
+            ListView lv = findViewById(R.id.bp_list);
+            lv.setAdapter(adapter);
+
+        }catch(Exception e){
+            Log.d("jammer", ""+e);
+        }
+    }
+
+    @Override
+    public void gotUniInfoError(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void CalculateEf(View v){
+        Intent intent = new Intent(this, CompareEfActivity.class);
+        intent.putExtra("efpc",min_efpc);
+        intent.putExtra("energy",min_energy);
+        intent.putExtra("mobility",min_mobility);
+        intent.putExtra("food",min_food);
+        intent.putExtra("waste",min_waste);
+        intent.putExtra("water",min_water);
+        intent.putExtra("built_land",min_built_land);
+        intent.putExtra("paper",min_paper);
+        startActivity(intent);
+    }
+}
